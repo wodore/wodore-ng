16606cdae0afa86025004473f0a41552bf9ecf8a
Tobias Burgherr <tburgherr@gmail.com>
2016-04-07 12:42:05 +0200

    Fixed admin page

--- main/public/modules/admin/config/admin.client.routes.js
+++ main/public/modules/admin/config/admin.client.routes.js
@@ -8,8 +8,24 @@
                 url         : '/admin',
                 abstract    : true,
                 controller  : 'AdminController',
-                templateUrl : '/p/modules/admin/layout/admin.client.view.html'
+                views: {
+                    'main@' : {
+                        templateUrl : '/p/modules/core/home/welcome.client.view.html'
+                    },
+                    'content@' : {
+                        templateUrl : '/p/modules/admin/layout/admin.client.view.html'
+                    },
+                    'header@' : {
+                        templateUrl : '/p/modules/core/home/header.home.view.html'
+                    }
+                }
             })
+            //.state('admin', {
+                //url         : '/admin',
+                //abstract    : true,
+                //controller  : 'AdminController',
+                //templateUrl : '/p/modules/admin/layout/admin.client.view.html'
+            //})
             .state('admin.appConfig', {
                 url         : '/config',
                 controller  : 'AdminAppConfigController',
